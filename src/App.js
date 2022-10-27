372b3c9ad23970578952bd4e4792344e29b453e3
bashar4980 <sagor.ahmed3669@gmail.com>
2022-10-27 16:20:12 +0600

    Navbar is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
 
-function App() {
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Main></Main>
+  }
+])
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/sharepages/Header.jsx
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+function Header() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Learn Tech</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Courses</Nav.Link>
+            <Nav.Link href="#pricing">FAQ</Nav.Link>
+            <Nav.Link href="#pricing">Blog</Nav.Link>
+            
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">User</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Logout
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
